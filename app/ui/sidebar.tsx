2528165cb8f27bc9d9e7d60630e082321946c58d
Wolfyre243 <wolfyreblueflare@gmail.com>
2024-04-08 00:21:55 +0800

    first commit

---
+++ app/ui/sidebar.tsx
@@ -0,0 +1,9 @@
+export default function SideBar(){
+    return (
+        <div className="p-6 bg-white text-black">
+            <p>This will be a link</p>
+            <p>This will also be a link</p>
+            <p>Yes this too</p>
+        </div>
+    )
+}
